44c74da3a3f0e6ea5291350b77a79b29103e78c6
guess <nik.shatov.05@mail.ru>
2026-01-31 18:35:42 +0300

    Обновленный начальный интерфейс!

--- frontend/script.js
+++ frontend/script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             {
                 id: 4,
-                title: "2058",
+                title: "2048 (моя игра)",
                 categories: ["Новые", "Головоломки"],
                 icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/2048_logo.svg/1200px-2048_logo.svg.png",
                 url: "games/2048v2/index.html"
